2b053019bccebdfd18528b88b9b7a3b66c0017c4
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2022-02-17 20:28:46 -0600

    js work

--- class12-materials/class-weekend-boring/js/main.js
+++ class12-materials/class-weekend-boring/js/main.js
@@ -1,10 +1,19 @@
-document.querySelector('#check').addEventListener('click', check)
+//addEventListener to grab value of #day, then compare value to days of week, tues&thur = class, sat&sun = weekend, else =boring
+
+document.querySelector('#check').addEventListener('click', check);
 
 function check() {
+	const day = document.querySelector('#day').value.toLowerCase();
 
-  const day = document.querySelector('#day').value
+	//Conditionals go here
 
-  //Conditionals go here
-
-
+	if (day === 'tuesday' || day === 'thursday') {
+		document.querySelector('#placeToSee').innerHTML = "It's Class Time Fool!";
+		console.log('Class Fool');
+	} else if (day === 'saturday' || day === 'sunday') {
+		document.querySelector('#placeToSee').innerHTML = 'Weekend is here!';
+		console.log('Weekend is here!');
+	} else {
+		document.querySelector('#placeToSee').innerHTML = 'Boooorrrring!';
+	}
 }
