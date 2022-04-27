9453753e63ccb6230174cc7c700deddbe8c72336
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2022-04-26 20:52:10 -0500

    add mod29

---
+++ class29-materials/cascade-badge/js/main.js
@@ -0,0 +1,40 @@
+//Arrays
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console.
+
+let pokemonPartyList = ['Castform', 'Rotom', 'Unown', 'blilander', 'Lycanroc'];
+
+function reverseArray(pokemonPartyList) {
+	console.log(pokemonPartyList.reverse());
+}
+
+reverseArray(pokemonPartyList);
+//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+function squareArr(arrA, arrB) {
+	return (
+		arrA.reduce((acc, c) => acc + c ** 2, 0) >
+		arrB.reduce((acc, c) => acc + c ** 3, 0)
+	);
+}
+console.log(squareArr([8, 8, 2], [2, 2, 2]));
+
+//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+function isMultiple(arr) {
+	return arr.filter((e, i) => e % i === 0);
+}
+
+console.log(isMultiple([22, -6, 32, 82, 9, 25]));
+
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+function allNumbers(arr) {
+	return arr.reduce((acc, c) => acc + Number(c), 0);
+}
+
+console.log(allNumbers(['3', 2, '9', 3]));
+
+const s = (r) => r.reduce((a, c) => +a + +c);
+console.log(s(['5', 3, '2', 1]));
